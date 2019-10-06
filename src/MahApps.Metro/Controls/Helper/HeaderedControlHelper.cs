6fb8c67bf22df2ebcf3bd2f7bca94132412d4371
timunie <47110241+timunie@users.noreply.github.com>
2019-10-06 15:31:13 +0200

    Rename GroupBoxHelper and Introduce new Attached Property
    - Rename GroupBoxHelper to HeaderedControlHelper as it applies to Expander also
    - Added a new Attached Property UseCornerRadiusAtHeader as a trigger for the styles

---
+++ src/MahApps.Metro/Controls/Helper/HeaderedControlHelper.cs
@@ -0,0 +1,44 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace MahApps.Metro.Controls
+{
+    using System.ComponentModel;
+
+    public static class HeaderedControlHelper
+    {
+        public static readonly DependencyProperty HeaderForegroundProperty =
+            DependencyProperty.RegisterAttached("HeaderForeground", typeof(Brush), typeof(HeaderedControlHelper), new UIPropertyMetadata(Brushes.White));
+
+        [Category(AppName.MahApps)]
+        [AttachedPropertyBrowsableForType(typeof(GroupBox))]
+        [AttachedPropertyBrowsableForType(typeof(Expander))]
+        public static Brush GetHeaderForeground(UIElement element)
+        {
+            return (Brush)element.GetValue(HeaderForegroundProperty);
+        }
+
+        public static void SetHeaderForeground(UIElement element, Brush value)
+        {
+            element.SetValue(HeaderForegroundProperty, value);
+        }
+
+
+        public static readonly DependencyProperty UseCornerRadiusAtHeaderProperty =
+            DependencyProperty.RegisterAttached("UseCornerRadiusAtHeader", typeof(bool), typeof(HeaderedControlHelper), new UIPropertyMetadata(false));
+
+        [Category(AppName.MahApps)]
+        [AttachedPropertyBrowsableForType(typeof(GroupBox))]
+        [AttachedPropertyBrowsableForType(typeof(Expander))]
+        public static bool GetUseCornerRadiusAtHeader(UIElement element)
+        {
+            return (bool)element.GetValue(UseCornerRadiusAtHeaderProperty);
+        }
+
+        public static void SetUseCornerRadiusAtHeader(UIElement element, bool value)
+        {
+            element.SetValue(UseCornerRadiusAtHeaderProperty, value);
+        }
+    }
+}
