1ad646232f49066aacd6afead7d3540e0575594f
timunie <47110241+timunie@users.noreply.github.com>
2019-11-21 20:17:21 +0100

    Initial idea

---
+++ src/MahApps.Metro/Controls/TextBoxButtonsContainer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+
+namespace MahApps.Metro.Controls
+{
+    [StyleTypedProperty(Property = "ItemContainerStyle", StyleTargetType = typeof(Button))]
+    public class TextBoxButtonsContainer : ItemsControl
+    {
+        protected override bool IsItemItsOwnContainerOverride(object item)
+        {
+            return item is Button;
+        }
+
+    }
+}
